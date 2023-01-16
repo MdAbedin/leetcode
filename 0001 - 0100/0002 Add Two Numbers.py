bd73e46673eae7d53b9df5e875aec425686b635f
Md Abedin <mdabedincs@gmail.com>
2023-01-15 22:00:23 -0500

    update 0002

--- 0001 - 0100/0002 Add Two Numbers.py
+++ 0001 - 0100/0002 Add Two Numbers.py
@@ -1,46 +1,30 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-def print_list(node):
-    if node is None:
-        print("NULL")
-    else:
-        print(node.val, end="->")
-        print_list(node.next)
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        curr1 = l1
-        curr2 = l2
-        carry = False
-        temp = ListNode(0)
-        temp.next = l1
-        while curr1 is not None and curr2 is not None:
-            curr1.val += curr2.val
-            if carry:
-                curr1.val += 1
-            carry = curr1.val >= 10
-            curr1.val %= 10
-            temp = curr1
-            curr1 = curr1.next
-            curr2 = curr2.next
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        sum_ = 0
         
-        if curr1 is None and curr2 is None:
-            if carry:
-                temp.next = ListNode(0)
-        elif curr1 is None:
-            temp.next = curr2
-        
-        print_list(l1)
-        while carry:
-            temp = temp.next
-            temp.val += 1
-            carry = temp.val >= 10
-            if temp.next is None and carry:
-                temp.next = ListNode(0)
-            temp.val %= 10
+        multiplier = 1
+        for node in takewhile(lambda x:x, accumulate(repeat(l1), lambda x,_:x.next)):
+            sum_ += multiplier * node.val
+            multiplier *= 10
             
-        return l1
+        multiplier = 1
+        for node in takewhile(lambda x:x, accumulate(repeat(l2), lambda x,_:x.next)):
+            sum_ += multiplier * node.val
+            multiplier *= 10
+            
+        if sum_ == 0: return ListNode(0)
+        
+        ans = ListNode()
+        cur = ans
+        
+        while sum_ > 0:
+            sum_, digit = divmod(sum_, 10)
+            cur.next = ListNode(digit)
+            cur = cur.next
+        
+        return ans.next
