49440b0c7f3b686f207b65b062966d2be7885e4e
Md Abedin <mdabedincs@gmail.com>
2023-01-15 22:02:24 -0500

    correct 0003

--- 0001 - 0100/0003 Longest Substring Without Repeating Characters.py
+++ 0001 - 0100/0003 Longest Substring Without Repeating Characters.py
@@ -1,20 +1,14 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        total_len = len(nums1) + len(nums2)
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
         
-        if total_len == 1: return (nums1+nums2)[0]
-        if total_len == 2: return (sum(nums1) + sum(nums2))/2
-        
-        def find_rank(k):
-            def works(num):
-                min_rank = bisect_left(nums1, num) + bisect_left(nums2, num)
-                max_rank = total_len-1 - (len(nums1)-bisect_right(nums1, num) + len(nums2)-bisect_right(nums2, num))
-                
-                return min_rank > k or max_rank >= k
+        for start_i in range(len(s)):
+            seen = set()
             
-            return -10**6 + bisect_left(range(-10**6,10**6+1), True, key = works)
-        
-        if total_len%2 == 1:
-            return find_rank(total_len//2)
-        else:
-            return (find_rank(total_len//2-1) + find_rank(total_len//2))/2
+            for end_i in range(start_i, len(s)):
+                if s[end_i] in seen: break
+                seen.add(s[end_i])
+                
+            ans = max(ans, len(seen))
+            
+        return ans
