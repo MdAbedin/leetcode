cb8fae7a036fbc0a324c7d49aa23f236b284e2ce
Md Abedin <mdabedincs@gmail.com>
2023-01-16 13:06:15 -0500

    add 0006

---
+++ 0001 - 0100/0006 Zigzag Conversion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1: return s
+        
+        ans = []
+        cycle_len = (numRows-1)*2
+        
+        for r in range(numRows):
+            if r in (0, numRows-1):
+                ans.append(s[r : len(s) : cycle_len])
+            else:
+                i = r
+                skip = cycle_len - 2*r
+                
+                while i < len(s):
+                    ans.append(s[i])
+                    i += skip
+                    skip = cycle_len - skip
+        
+        return "".join(ans)
