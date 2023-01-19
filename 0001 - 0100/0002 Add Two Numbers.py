c82b73f7998618b627b66058247bd36c0cbe5f7d
Md Abedin <mdabedincs@gmail.com>
2023-01-19 16:01:56 -0500

    update 0002 with complexities

--- 0001 - 0100/0002 Add Two Numbers.py
+++ 0001 - 0100/0002 Add Two Numbers.py
@@ -1,9 +1,10 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
+    """
+    N1 = number of nodes in l1
+    N2 = number of nodes in l2    
+    Time:  O(max(N1, N2))
+    Space: O(max(N1, N2))
+    """
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         sum_ = 0
         
