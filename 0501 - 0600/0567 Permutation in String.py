8cdd6a8f76f82780e2b25babf034d212ecb72850
Md Abedin <mdabedincs@gmail.com>
2023-02-03 22:32:32 -0500

    update 0567

--- 0501 - 0600/0567 Permutation in String.py
+++ 0501 - 0600/0567 Permutation in String.py
@@ -1,14 +1,21 @@
 class Solution:
+
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        counts1 = [s1.count(chr(ord('a') + i)) for i in range(26)]
-        counts2 = [s2[:len(s1)].count(chr(ord('a') + i)) for i in range(26)]
-        ans = counts2 == counts1
-        if ans: return True
+
+        c1 = Counter(s1)
+
+        c2 = Counter(s2[:len(s1)-1])
+
         
-        for i in range(1,len(s2)-len(s1)+1):
-            counts2[ord(s2[i-1]) - ord('a')] -= 1
-            counts2[ord(s2[i+len(s1)-1]) - ord('a')] += 1
+
+        for i in range(len(s1)-1,len(s2)):
+
+            c2[s2[i]] += 1
+
+            if c2 == c1: return True
+
+            c2[s2[i-len(s1)+1]] -= 1
+
             
-            if counts2 == counts1: return True
-                
+
         return False
