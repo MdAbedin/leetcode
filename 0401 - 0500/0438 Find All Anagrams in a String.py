24486526f52b4d29aee961919c1e3538345ac1da
Md Abedin <mdabedincs@gmail.com>
2023-02-04 22:41:43 -0500

    add 0438

--- 0401 - 0500/0438 Find All Anagrams in a String.py
+++ 0401 - 0500/0438 Find All Anagrams in a String.py
@@ -1,13 +1,12 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p_counts = [p.count(chr(ord('a') + i)) for i in range(26)]
-        s_counts = [s[:len(p)].count(chr(ord('a') + i)) for i in range(26)]
-        ans = [0] if p_counts == s_counts else []
-        
-        for i in range(1, len(s)-len(p)+1):
-            s_counts[ord(s[i-1]) - ord('a')] -= 1
-            s_counts[ord(s[i+len(p)-1]) - ord('a')] += 1
-            if s_counts == p_counts:
-                ans.append(i)
-        
+        c1 = Counter(p)
+        c2 = Counter(s[:len(p)-1])
+        ans = []
+
+        for i in range(len(p)-1,len(s)):
+            c2[s[i]] += 1
+            if c2 == c1: ans.append(i-len(p)+1)
+            c2[s[i-len(p)+1]] -= 1
+
         return ans
