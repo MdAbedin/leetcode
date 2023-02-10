30ef2d573c45e2f0dbda3523e27939f50b958bb2
Md Abedin <mdabedincs@gmail.com>
2023-02-09 23:47:44 -0500

    update 1162

--- 1101 - 1200/1162 As Far from Land as Possible.py
+++ 1101 - 1200/1162 As Far from Land as Possible.py
@@ -8,9 +8,11 @@ class Solution:
             for r in range(R):
                 if grid[r][c] == 0:
                     cur_ans = inf
+
                     for r2,row_land_cells in enumerate(land_cells):
                         if len(row_land_cells) >= 2 and abs(c - row_land_cells[1]) <= abs(c - row_land_cells[0]): row_land_cells.popleft()
                         if row_land_cells: cur_ans = min(cur_ans, abs(r-r2) + abs(c - row_land_cells[0]))
+                        
                     if cur_ans != inf: ans = max(ans, cur_ans)
 
         return ans
