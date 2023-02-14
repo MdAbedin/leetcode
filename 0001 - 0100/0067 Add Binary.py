995faad31041d1f53938fdbd81e32865079f3eb5
Md Abedin <mdabedincs@gmail.com>
2023-02-13 21:17:21 -0500

    update 0067

--- 0001 - 0100/0067 Add Binary.py
+++ 0001 - 0100/0067 Add Binary.py
@@ -1,5 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-      max_len = max(len(a), len(b))
-      
-      return bin(int('0'*(max_len-len(a))+a, 2) + int('0'*(max_len-len(b))+b, 2))[2:]
+        return f"{int(a,2)+int(b,2):b}"
