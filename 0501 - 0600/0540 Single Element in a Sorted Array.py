4029df44f1df730db927a306d56936ce3a2b03f4
Md Abedin <mdabedincs@gmail.com>
2023-02-20 21:05:30 -0500

    update 0540

--- 0501 - 0600/0540 Single Element in a Sorted Array.py
+++ 0501 - 0600/0540 Single Element in a Sorted Array.py
@@ -1,8 +1,18 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        for i in range(0,len(nums),2):
-            if i == len(nums)-1:
-                return nums[i]
-            
-            if nums[i] != nums[i+1]:
-                return nums[i]
+        l,r = 0,len(nums)-1
+
+        while l <= r:
+            m = (l+r)//2
+
+            if m-1 >= 0 and nums[m-1] == nums[m]:
+                first_i = m-1
+            elif m+1 < len(nums) and nums[m+1] == nums[m]:
+                first_i = m
+            else:
+                return nums[m]
+
+            if first_i % 2 == 0:
+                l = m+1
+            else:
+                r = m-1
