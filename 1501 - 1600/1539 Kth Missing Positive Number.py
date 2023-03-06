49eda8e720f0183f0b0c4d2970a3ea6d445a3baf
Md Abedin <mdabedincs@gmail.com>
2023-03-05 22:48:37 -0500

    update 1539

--- 1501 - 1600/1539 Kth Missing Positive Number.py
+++ 1501 - 1600/1539 Kth Missing Positive Number.py
@@ -1,12 +1,7 @@
 class Solution:
+
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        ans = 1
-        
-        for num in arr:
-          while ans != num:
-            k -= 1
-            if k == 0: return ans
-            ans += 1
-          ans += 1
-            
-        return ans+k-1
+
+        arr = set(arr)
+
+        return [num for num in range(1,2001) if num not in arr][k-1]
