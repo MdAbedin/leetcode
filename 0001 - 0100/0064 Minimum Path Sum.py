35464b03c853ab9e9efdf66c956465674bc1741a
Md Abedin <mdabedincs@gmail.com>
2023-03-26 20:28:49 -0400

    update 0064

--- 0001 - 0100/0064 Minimum Path Sum.py
+++ 0001 - 0100/0064 Minimum Path Sum.py
@@ -1,14 +1,9 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if y == 0 and x == 0:
-                    continue
-                if y-1 >= 0 and x-1 >= 0:
-                    grid[y][x] += min(grid[y-1][x], grid[y][x-1])
-                elif y-1 >= 0:
-                    grid[y][x] += grid[y-1][x]
-                else:
-                    grid[y][x] += grid[y][x-1]
-                
-        return grid[-1][-1]
+        dp = list(accumulate(grid[0]))
+
+        for r in range(1,len(grid)):
+            for c in range(len(grid[0])):
+                dp[c] = grid[r][c] + min(dp[c], dp[c-1] if c-1 >= 0 else inf)
+
+        return dp[-1]
