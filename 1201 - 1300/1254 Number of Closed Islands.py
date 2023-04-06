74a6641f16e88877bcdd135edc0b65a280eeb306
Md Abedin <mdabedincs@gmail.com>
2023-04-05 23:21:39 -0400

    update 1254

--- 1201 - 1300/1254 Number of Closed Islands.py
+++ 1201 - 1300/1254 Number of Closed Islands.py
@@ -1,27 +1,55 @@
 class Solution:
+
     def closedIsland(self, grid: List[List[int]]) -> int:
+
+        R,C = len(grid),len(grid[0])
+
+        seen = set()
+
         ans = 0
+
         
-        for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if grid[y][x] == 0:
-                    bfs = deque([[y,x]])
-                    grid[y][x] = -1
-                    is_closed = True
+
+        for r in range(R):
+
+            for c in range(C):
+
+                if (r,c) in seen or grid[r][c] == 1: continue
+
                     
-                    while bfs:
-                        cur_y, cur_x = bfs.popleft()
-                        
-                        for dy, dx in [[0,1],[1,0],[0,-1],[-1,0]]:
-                            new_y, new_x = cur_y+dy, cur_x+dx
-                            
-                            if new_y < 0 or new_y >= len(grid) or new_x < 0 or new_x >= len(grid[0]):
-                                is_closed = False
-                            else:
-                                if grid[new_y][new_x] == 0:
-                                    grid[new_y][new_x] = -1
-                                    bfs.append([new_y, new_x])
-                            
-                    ans += 1 if is_closed else 0
+
+                seen.add((r,c))
+
+                dfs = [[r,c]]
+
+                closed = True
+
                 
+
+                while dfs:
+
+                    cr,cc = dfs.pop()
+
+                    
+
+                    for r2,c2 in [[cr+1,cc],[cr-1,cc],[cr,cc+1],[cr,cc-1]]:
+
+                        if r2 not in range(R) or c2 not in range(C):
+
+                            closed = False
+
+                        else:
+
+                            if (r2,c2) not in seen and grid[r2][c2] == 0:
+
+                                seen.add((r2,c2))
+
+                                dfs.append([r2,c2])
+
+                                
+
+                if closed: ans += 1
+
+                    
+
         return ans
