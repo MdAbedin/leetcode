122334a1a01a7e2ca87ea41a9814d495f5c420fd
Md Abedin <mdabedincs@gmail.com>
2023-04-10 23:35:05 -0400

    add 2390

---
+++ 2301 - 2400/2390 Removing Stars From a String.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def removeStars(self, s: str) -> str:
+
+        stack = []
+
+        
+
+        for c in s:
+
+            if c == "*":
+
+                stack.pop()
+
+            else:
+
+                stack.append(c)
+
+                
+
+        return "".join(stack)
