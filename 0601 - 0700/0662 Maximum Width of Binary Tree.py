4cb578885f007615ccdfb4e8a9fde67c4d70821e
Md Abedin <mdabedincs@gmail.com>
2023-04-19 22:51:11 -0400

    update 0662

--- 0601 - 0700/0662 Maximum Width of Binary Tree.py
+++ 0601 - 0700/0662 Maximum Width of Binary Tree.py
@@ -1,29 +1,49 @@
 # Definition for a binary tree node.
+
 # class TreeNode:
+
 #     def __init__(self, val=0, left=None, right=None):
+
 #         self.val = val
+
 #         self.left = left
+
 #         self.right = right
+
 class Solution:
-    def widthOfBinaryTree(self, root: TreeNode) -> int:
-        if not root: return 0
+
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        mins = defaultdict(lambda: inf)
+
+        maxes = defaultdict(lambda: -inf)
+
         
-        bfs = deque([[root,1]])
-        ans = 1
-        
-        while bfs:
-          l,r = -1,-1
-          n = len(bfs)
-          
-          for i in range(n):
-            cur, idx = bfs.popleft()
+
+        def dfs(node,level,i):
+
+            if not node: return
+
             
-            if i == 0: l = idx
-            if i == n-1: r = idx
-              
-            if cur.left: bfs.append([cur.left, idx*2])
-            if cur.right: bfs.append([cur.right, idx*2+1])
-              
-          ans = max(ans, r-l+1)
-          
-        return ans
+
+            mins[level] = min(mins[level],i)
+
+            maxes[level] = max(maxes[level],i)
+
+            
+
+            dfs(node.left,level+1,2*i)
+
+            dfs(node.right,level+1,2*i+1)
+
+            
+
+        dfs(root,1,1)
+
+        
+
+        return max(maxes[level]-mins[level]+1 for level in mins)
+
+            
+
+            
