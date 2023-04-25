bb3bb7cca0e986c7e514b06b66070e431ac22e1a
Md Abedin <mdabedincs@gmail.com>
2023-04-24 22:12:58 -0400

    update 1046

--- 1001 - 1100/1046 Last Stone Weight.py
+++ 1001 - 1100/1046 Last Stone Weight.py
@@ -1,35 +1,10 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        counts = [0]*1001
-        for weight in stones:
-                counts[weight] += 1
-            
-        while True:
-            found = 0
-            max1 = -1
-            max2 = -1
-            i = 1000
-            
-            while i:
-                if counts[i]:
-                    found += 1
-                    counts[i] -= 1
-                    
-                    if found == 1:
-                        max1 = i
-                    else:
-                        max2 = i
-                    
-                if found == 2:
-                    break
-                
-                if counts[i]:
-                    i += 1
-                    
-                i -= 1
-                
-            if found == 2:
-                if max1 != max2:
-                    counts[max1-max2] += 1
-            else:
-                return max1 if found == 1 else 0
+        pq = list(map(neg,stones))
+        heapify(pq)
+
+        while len(pq) > 1:
+            y,x = -heappop(pq),-heappop(pq)
+            if y != x: heappush(pq, -(y-x))
+
+        return 0 if not pq else -pq[0]
