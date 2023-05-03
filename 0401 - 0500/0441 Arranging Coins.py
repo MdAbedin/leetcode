79ff5a0b628ed2a378208457835678a925b56b0c
Md Abedin <mdabedincs@gmail.com>
2023-05-03 13:08:10 -0400

    update 0441

--- 0401 - 0500/0441 Arranging Coins.py
+++ 0401 - 0500/0441 Arranging Coins.py
@@ -1,3 +1,10 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((-1 + sqrt(1 - 4*-2*n))/2)
+        coins_used = 0
+        complete_rows = 0
+
+        while coins_used + complete_rows+1 <= n:
+            coins_used += complete_rows+1
+            complete_rows += 1
+
+        return complete_rows
