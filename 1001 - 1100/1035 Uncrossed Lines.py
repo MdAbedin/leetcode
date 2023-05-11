38cf1ca76c257b3bca654f2b521ef28b9719adda
Md Abedin <mdabedincs@gmail.com>
2023-05-10 20:44:52 -0400

    update 1035

--- 1001 - 1100/1035 Uncrossed Lines.py
+++ 1001 - 1100/1035 Uncrossed Lines.py
@@ -1,13 +1,8 @@
 class Solution:
-    def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
-        buf = [0]*len(A)
-        
-        for i in range(len(B)):
-            buf2 = [0]*len(A)
-            
-            for j in range(len(A)):
-                buf2[j] = max(buf2[j-1] if j-1>=0 else 0, buf[j], (buf[j-1] if j-1>= 0 else 0) + 1 if A[j]==B[i] else 0)
-                
-            buf = buf2
-            
-        return buf[-1]
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        @cache
+        def solve(i,j):
+            if i == len(nums1) or j == len(nums2): return 0
+            return 1+solve(i+1,j+1) if nums1[i] == nums2[j] else max(solve(i+1,j),solve(i,j+1))
+
+        return solve(0,0)
