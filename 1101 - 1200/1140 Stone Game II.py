c62d537893fc87bb21d66961768c10d1c2116216
Md Abedin <mdabedincs@gmail.com>
2023-05-25 20:35:42 -0400

    add 1140

---
+++ 1101 - 1200/1140 Stone Game II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        @cache
+        def solve(i,M):
+            if i >= len(piles): return 0,0
+
+            ans = (0,0)
+
+            for x in range(1,2*M+1):
+                s0,s1 = solve(i+x,max(M,x))
+                if sum(piles[i:i+x])+s1 > ans[0]: ans = sum(piles[i:i+x])+s1,s0
+
+            return ans
+
+        return solve(0,1)[0]
