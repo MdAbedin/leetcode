0597e509f5e5cebafc8de7a45d7f9e319366bf70
Md Abedin <mdabedincs@gmail.com>
2023-06-04 20:13:55 -0400

    update 1232

--- 1201 - 1300/1232 Check If It Is a Straight Line.py
+++ 1201 - 1300/1232 Check If It Is a Straight Line.py
@@ -1,16 +1,3 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        x1,y1 = coordinates[0]
-        x2,y2 = coordinates[1]
-        
-        if x2 == x1:
-            return all(x == x1 for x,y in coordinates)
-        
-        m = (y2-y1)/(x2-x1)
-        b = y1 - m*x1
-        
-        for x,y in coordinates:
-            if y != m*x + b:
-                return False
-            
-        return True
+        return len({None if x1==x2 else (y2-y1)/(x2-x1) for (x1,y1),(x2,y2) in pairwise(coordinates)}) == 1
