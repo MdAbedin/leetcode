6ef875cf6bca28da3797c2a9f02fd581a5fa6444
Md Abedin <mdabedincs@gmail.com>
2023-06-12 16:33:27 -0400

    add 0228

--- 0201 - 0300/0228 Summary Ranges.py
+++ 0201 - 0300/0228 Summary Ranges.py
@@ -1,22 +1,9 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums: return []
-        
         ans = []
-        prev_start = nums[0]
 
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1] + 1:
-                if nums[i-1] == prev_start:
-                    ans.append(str(prev_start))
-                else:
-                    ans.append(f"{prev_start}->{nums[i-1]}")
-                
-                prev_start = nums[i]
+        for num in nums:
+            if ans and num==ans[-1][1]+1: ans[-1][1] = num
+            else: ans.append([num,num])
 
-        if prev_start == nums[-1]:
-            ans.append(str(prev_start))
-        else:
-            ans.append(f"{prev_start}->{nums[-1]}")
-
-        return ans
+        return [str(l) if l==r else f"{l}->{r}" for l,r in ans]
