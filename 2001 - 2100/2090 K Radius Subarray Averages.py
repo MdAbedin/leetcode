9165835d9b2d5149ab71422328a52e6cca8b200f
Md Abedin <mdabedincs@gmail.com>
2023-06-19 22:37:56 -0400

    update 2090

--- 2001 - 2100/2090 K Radius Subarray Averages.py
+++ 2001 - 2100/2090 K Radius Subarray Averages.py
@@ -1,5 +1,4 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
-        psums = [0] + list(accumulate(nums))
-
-        return [-1 if (i-k < 0 or i+k >= len(nums)) else (int((psums[i+k+1] - psums[i-k]) / (2*k+1))) for i in range(len(nums))]
+        psums = [0]+list(accumulate(nums))
+        return [-1 if i-k < 0 or i+k >= len(nums) else int((psums[i+k+1]-psums[i-k])/(2*k+1)) for i in range(len(nums))]
