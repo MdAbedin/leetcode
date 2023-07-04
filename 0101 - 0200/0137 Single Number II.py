0b7ba50f173f08429cc9c7e90ab6313201e5fe1d
Md Abedin <mdabedincs@gmail.com>
2023-07-04 19:06:57 -0400

    add 0137

--- 0101 - 0200/0137 Single Number II.py
+++ 0101 - 0200/0137 Single Number II.py
@@ -1,17 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        min_num = min(0, min(nums))
-        
-        for i in range(len(nums)):
-            nums[i] -= min_num
-            
-        max_bin = ['0']*len(bin(max(nums))[2:])
-        
-        for num in nums:
-            num_bin = bin(num)[2:]
-            num_bin = '0'*(len(max_bin)-len(num_bin)) + num_bin
-            
-            for i in range(len(max_bin)):
-                max_bin[i] = str((int(max_bin[i]) + int(num_bin[i])) % 3)
-                
-        return int(''.join(max_bin), base=2)+min_num
+        ans = sum(1<<i for i in range(32) if sum((1 if num&(1<<i) else 0) for num in nums)%3 == 1)
+        return ans-(1<<32) if ans >= (1<<31) else ans
