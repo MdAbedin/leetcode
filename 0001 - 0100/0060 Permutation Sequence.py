0aa1c63836b440a887c7617eadba5ba8a4199a71
Md Abedin <mdabedincs@gmail.com>
2023-07-06 10:23:46 -0400

    update 0060

--- 0001 - 0100/0060 Permutation Sequence.py
+++ 0001 - 0100/0060 Permutation Sequence.py
@@ -1,16 +1,11 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        factorials = [1]*n
-        for i in range(1,len(factorials)): factorials[i] = (i+1)*factorials[i-1]
-        
-        ans = ''
-        digits = [str(i) for i in range(1, n+1)]
         k -= 1
-        
-        for i in range(n-1):
-            group = k//factorials[-1*i-2]
-            ans += digits[group]
-            del digits[group]
-            k = k%factorials[-1*i-2]
-            
-        return ans+digits[0]
+        nums = list(range(1,n+1))
+        ans = []
+
+        for i in range(n):
+            ans.append(nums.pop(k//factorial(n-i-1)))
+            k %= factorial(n-i-1)
+
+        return "".join(map(str,ans))
