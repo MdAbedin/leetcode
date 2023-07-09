4051219357ada43b87b9a31d8a2668f3e22b2f64
Md Abedin <mdabedincs@gmail.com>
2023-07-09 13:42:35 -0400

    add 0063

--- 0001 - 0100/0063 Unique Paths II.py
+++ 0001 - 0100/0063 Unique Paths II.py
@@ -1,25 +1,10 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        if not obstacleGrid:
-            return 0
-        if not obstacleGrid[0]:
-            return 1
-        
-        if obstacleGrid[0][0] == 1:
-            return 0
-        
-        for y in range(len(obstacleGrid)):
-            for x in range(len(obstacleGrid[0])):
-                if y == 0 and x == 0:
-                    obstacleGrid[0][0] = 1
-                    continue
-                    
-                if obstacleGrid[y][x] == 0:
-                    if y-1 >= 0 and obstacleGrid[y-1][x] >= 0:
-                        obstacleGrid[y][x] += obstacleGrid[y-1][x]
-                    if x-1 >= 0 and obstacleGrid[y][x-1] >= 0:
-                        obstacleGrid[y][x] += obstacleGrid[y][x-1]
-                else:
-                    obstacleGrid[y][x] = -1
-        
-        return obstacleGrid[-1][-1] if obstacleGrid[-1][-1] >= 0 else 0
+        dp = [1] + [0]*(len(obstacleGrid[0])-1)
+
+        for row in obstacleGrid:
+            dp2 = []
+            for obstacle,above in zip(row,dp): dp2.append(0 if obstacle else (above + (dp2[-1] if dp2 else 0)))
+            dp = dp2
+
+        return dp[-1]
