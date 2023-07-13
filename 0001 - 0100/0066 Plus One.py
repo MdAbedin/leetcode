e6ce52136ee78eb682e60c001054864047bf4fa8
Md Abedin <mdabedincs@gmail.com>
2023-07-13 13:25:14 -0400

    update 0066

--- 0001 - 0100/0066 Plus One.py
+++ 0001 - 0100/0066 Plus One.py
@@ -1,3 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-      return list(str(int(''.join(map(str, digits)))+1))
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] != 9:
+                digits[i] += 1
+                digits[i+1:] = [0]*(len(digits)-i-1)
+                return digits
+        
+        return [1] + [0]*len(digits)
