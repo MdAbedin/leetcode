23336736fc971085d08317af052b094c39e92c4f
Md Abedin <mdabedincs@gmail.com>
2023-07-12 20:39:00 -0400

    update 0207

--- 0201 - 0300/0207 Course Schedule.py
+++ 0201 - 0300/0207 Course Schedule.py
@@ -1,25 +1,19 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        neighbors = defaultdict(set)
-        in_degrees = defaultdict(int)
-        edges = len(prerequisites)
-        
+        prereqs_left = Counter()
+        G2 = defaultdict(list)
+
         for a,b in prerequisites:
-            neighbors[a].add(b)
-            in_degrees[b] += 1
-            
-        tsort = deque([i for i in range(numCourses) if i not in in_degrees])
-        
+            prereqs_left[a] += 1
+            G2[b].append(a)
+
+        tsort = [i for i in range(numCourses) if prereqs_left[i] == 0]
+
         while tsort:
-            cur = tsort.popleft()
-            remove = set()
-            
-            for neighbor in neighbors[cur]:
-                in_degrees[neighbor] -= 1
-                edges -= 1
-                if in_degrees[neighbor] == 0:
-                    tsort.append(neighbor)
-                remove.add(neighbor)
-            neighbors[cur] -= remove
-                
-        return edges == 0
+            node = tsort.pop()
+
+            for nei in G2[node]:
+                prereqs_left[nei] -= 1
+                if prereqs_left[nei] == 0: tsort.append(nei)
+
+        return set(prereqs_left.values()).issubset({0})
