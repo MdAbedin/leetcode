743d01e3b5d364b396e29dece640b7f4aeb149aa
Md Abedin <mdabedincs@gmail.com>
2023-07-13 20:13:44 -0400

    update 1218

--- 1201 - 1300/1218 Longest Arithmetic Subsequence of Given Difference.py
+++ 1201 - 1300/1218 Longest Arithmetic Subsequence of Given Difference.py
@@ -1,10 +1,5 @@
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
-        lens = defaultdict(int)
-        ans = 0
-        
-        for num in arr:
-            lens[num] = lens[num-difference]+1
-            ans = max(ans, lens[num])
-            
-        return ans
+        ans = defaultdict(int)
+        for num in arr[::-1]: ans[num] = 1+ans[num+difference]
+        return max(ans.values())
