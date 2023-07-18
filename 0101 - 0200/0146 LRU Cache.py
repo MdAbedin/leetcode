6d53d8f788067086853b16482ea1443da73c8b32
Md Abedin <mdabedincs@gmail.com>
2023-07-17 21:03:14 -0400

    update 0146

--- 0101 - 0200/0146 LRU Cache.py
+++ 0101 - 0200/0146 LRU Cache.py
@@ -1,59 +1,49 @@
-class DoublyLinkedList:
-    class Node:
-        def __init__(self, data = None):
-            self.data = data
-            self.prev = None
-            self.next = None
-            
+class Node:
+    def __init__(self, key = None, val = None, prev = None, nxt = None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.nxt = nxt
+
+class DLL:
     def __init__(self):
-        self.head = self.Node()
-        self.tail = self.Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.length = 0
-        
-    def __len__(self):
-        return self.length
+        self.head = Node()
+        self.tail = Node()
+        self.head.nxt = self.tail
+        self.tail.prev = self.head 
     
-    def remove(self, node):
-        self.length -= 1
-        node.prev.next, node.next.prev = node.next, node.prev
+    def pop(self, node = None):
+        if not node: node = self.tail.prev
+        if node == self.head: return None
+
+        node.prev.nxt = node.nxt
+        node.nxt.prev = node.prev
+
+        return node      
+
+    def add_first(self, node):
+        node.nxt = self.head.nxt
+        node.prev = self.head
+        
+        node.prev.nxt = node
+        node.nxt.prev = node
         
         return node
-
-    def append(self, data):
-        new_node = self.Node(data)
-        new_node.prev = self.tail.prev
-        new_node.next = self.tail
-        new_node.prev.next = new_node
-        new_node.next.prev = new_node
-        self.length += 1
-        
-        return new_node
-    
-    def popleft(self):
-        return self.remove(self.head.next)
     
 class LRUCache:
     def __init__(self, capacity: int):
+        self.key_to_node = {}
+        self.DLL = DLL()
         self.capacity = capacity
-        self.ll = DoublyLinkedList()
-        self.maps = dict()
 
-    def get(self, key: int) -> int:
-        return self.put(key, self.maps[key].data[1]) if key in self.maps else -1
+    def get_node(self, key):
+        return None if key not in self.key_to_node else self.DLL.add_first(self.DLL.pop(self.key_to_node[key]))
     
+    def get(self, key: int) -> int:
+        return node.val if (node := self.get_node(key)) else -1
+
     def put(self, key: int, value: int) -> None:
-        if key in self.maps:
-            self.maps[key] = self.ll.append([self.ll.remove(self.maps[key]).data[0], value])
-            return value
-        else:
-            if len(self.ll) == self.capacity:
-                del self.maps[self.ll.popleft().data[0]]
+        if key not in self.key_to_node: self.key_to_node[key] = self.DLL.add_first(Node(key,value))
+        else: self.get_node(key).val = value
             
-            self.maps[key] = self.ll.append([key,value])
-            
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        if len(self.key_to_node) > self.capacity: self.key_to_node.pop(self.DLL.pop().key)
