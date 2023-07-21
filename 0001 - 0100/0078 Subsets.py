c46ceebf41c61973eb5b8d94a5236786599fa541
Md Abedin <mdabedincs@gmail.com>
2023-07-21 13:54:11 -0400

    update 0078

--- 0001 - 0100/0078 Subsets.py
+++ 0001 - 0100/0078 Subsets.py
@@ -1,11 +1,3 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-      ans = []
-      
-      for i in range(2**len(nums)):
-        bitmask = bin(i)[2:]
-        bitmask = '0'*(len(nums)-len(bitmask)) + bitmask
-        
-        ans.append([nums[j] for j in range(len(nums)) if bitmask[j] == '1'])
-        
-      return ans
+        return [subset for size in range(len(nums)+1) for subset in combinations(nums,size)]
