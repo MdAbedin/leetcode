9bd370e281a70d4952b5c348706a68b84464b0fd
Md Abedin <mdabedincs@gmail.com>
2023-07-21 20:56:23 -0400

    update 0688

--- 0601 - 0700/0688 Knight Probability in Chessboard.py
+++ 0601 - 0700/0688 Knight Probability in Chessboard.py
@@ -1,19 +1,17 @@
 class Solution:
-    def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
-        grid = [[0]*(N+4) for i in range(N+4)]
-        for y in range(2,2+N):
-            for x in range(2,2+N):
-                grid[y][x] = 1
-            
-        moves = [[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]
-        for i in range(K):
-            cp = [[0]*(N+4) for i in range(N+4)]
-            
-            for y in range(2,2+N):
-                for x in range(2,2+N):
-                    for dy,dx in moves:
-                        cp[y][x] += 1/8*grid[y+dy][x+dx]
-                        
-            grid = cp
-                        
-        return grid[2+r][2+c]
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        probs = [[0]*n for i in range(n)]
+        probs[row][column] = 1
+
+        for i in range(k):
+            probs2 = [[0]*n for i in range(n)]
+
+            for r in range(n):
+                for c in range(n):
+                    for r2,c2 in [[r+1,c+2],[r+1,c-2],[r-1,c+2],[r-1,c-2],[r+2,c+1],[r+2,c-1],[r-2,c+1],[r-2,c-1]]:
+                        if r2 in range(n) and c2 in range(n):
+                            probs2[r2][c2] += probs[r][c]/8
+
+            probs = probs2
+
+        return sum(map(sum,probs))
