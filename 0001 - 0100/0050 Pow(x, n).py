8ffb67e2f9e76082054bac127b260e2222cbf98c
Md Abedin <mdabedincs@gmail.com>
2023-07-23 21:01:23 -0400

    update 0050

--- 0001 - 0100/0050 Pow(x, n).py
+++ 0001 - 0100/0050 Pow(x, n).py
@@ -1,3 +1,6 @@
 class Solution:
+    @cache
     def myPow(self, x: float, n: int) -> float:
-      return x ** n
+        if n == 0: return 1
+        if n < 0: return 1/self.myPow(x,-n)
+        return self.myPow(x,n//2)*self.myPow(x,n//2)*(x if n%2 == 1 else 1)
