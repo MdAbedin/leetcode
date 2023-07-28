6a63e89952a17722a8e3c9e16203ef2f01b9c2b4
Md Abedin <mdabedincs@gmail.com>
2023-07-27 20:14:25 -0400

    update 0486

--- 0401 - 0500/0486 Predict the Winner.py
+++ 0401 - 0500/0486 Predict the Winner.py
@@ -1,12 +1,16 @@
 class Solution:
-    def helper(self, l, r):
-        if l > r: return 0
-        
-        shared = self.helper(l+1, r-1)
-        
-        return max(self.nums[l] + min(self.helper(l+2, r), shared), self.nums[r] + min(shared, self.helper(l, r-2)))
-    
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        self.nums = nums
-        
-        return self.helper(0, len(nums)-1) >= min(self.helper(1, len(nums)-1), self.helper(0, len(nums)-2))
+        def solve(nums):
+            if not nums: return 0,0
+
+            l_score2,l_score1 = solve(nums[1:])
+            l_score1 += nums[0]
+
+            r_score2,r_score1 = solve(nums[:-1])
+            r_score1 += nums[-1]
+
+            return (l_score1,l_score2) if l_score1 >= r_score1 else (r_score1,r_score2)
+
+        score1,score2 = solve(nums)
+
+        return score1 >= score2
