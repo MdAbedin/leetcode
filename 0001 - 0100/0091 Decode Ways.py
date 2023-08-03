0ac8c89ed81a0943891d35ac99658bbc5b3b9b31
Md Abedin <mdabedincs@gmail.com>
2023-08-03 19:48:45 -0400

    update 0091

--- 0001 - 0100/0091 Decode Ways.py
+++ 0001 - 0100/0091 Decode Ways.py
@@ -1,10 +1,7 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [0]*len(s)
-        if s[0] != '0': dp[0] = 1
-            
-        for i in range(1,len(s)):
-            if s[i] != '0': dp[i] += dp[i-1]
-            if s[i-1] != '0' and int(s[i-1:i+1]) <= 26: dp[i] += dp[i-2] if i-2>=0 else 1
-                
-        return dp[-1]
+        cur,prev = 1,0
+        
+        for i in range(len(s)): cur,prev = (cur if int(s[i]) in range(1,10) else 0) + (prev if i-1 >= 0 and s[i-1] != "0" and int(s[i-1:i+1]) in range(1,27) else 0),cur
+
+        return cur
