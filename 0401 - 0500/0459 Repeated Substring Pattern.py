fff601b523f5827c7cc09ad402a86b51ac8af9d9
Md Abedin <mdabedincs@gmail.com>
2023-08-20 23:00:21 -0400

    update 0459

--- 0401 - 0500/0459 Repeated Substring Pattern.py
+++ 0401 - 0500/0459 Repeated Substring Pattern.py
@@ -1,8 +1,3 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        for length in range(1,len(s)):
-            if len(s) % length == 0:
-                if all(s[length*m : length*(m+1)] == s[:length] for m in range(len(s)//length)):
-                    return True
-
-        return False
+        return any(all(s[i] == s[i%length] for i in range(len(s))) for length in range(1,len(s)) if len(s)%length == 0)
