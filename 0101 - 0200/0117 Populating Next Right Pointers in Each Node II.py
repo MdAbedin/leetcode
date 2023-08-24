2cf7f6173ac55d6cdf9830528e347ac64c559bff
Md Abedin <mdabedincs@gmail.com>
2023-08-24 19:36:24 -0400

    update 0117

--- 0101 - 0200/0117 Populating Next Right Pointers in Each Node II.py
+++ 0101 - 0200/0117 Populating Next Right Pointers in Each Node II.py
@@ -1,29 +1,25 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.next = next
-"""
-
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return None
-        
-        bfs = deque([root])
-        
-        while bfs:
-            length = len(bfs)
-            for i in range(length):
-                cur = bfs.popleft()
-                cur.next = bfs[0] if i < length-1 else None
-                
-                if cur.left:
-                    bfs.append(cur.left)
-                if cur.right:
-                    bfs.append(cur.right)
-                    
+        cur_level_first_node = root
+
+        while cur_level_first_node:
+            cur_node = cur_level_first_node
+            next_level_first_node = None
+            next_level_prev_node = None
+
+            while cur_node:
+                if cur_node.left:
+                    if not next_level_first_node: next_level_first_node = cur_node.left
+                    if next_level_prev_node: next_level_prev_node.next = cur_node.left
+                    next_level_prev_node = cur_node.left
+
+                if cur_node.right:
+                    if not next_level_first_node: next_level_first_node = cur_node.right
+                    if next_level_prev_node: next_level_prev_node.next = cur_node.right
+                    next_level_prev_node = cur_node.right
+
+                cur_node = cur_node.next
+
+            cur_level_first_node = next_level_first_node
+
         return root
