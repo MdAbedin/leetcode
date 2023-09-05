0f006bfe46bae9048f77c11d3e4ff72306ab411c
Md Abedin <mdabedincs@gmail.com>
2023-09-05 08:45:33 -0400

    update 0134

--- 0101 - 0200/0134 Gas Station.py
+++ 0101 - 0200/0134 Gas Station.py
@@ -1,16 +1,19 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        ans, gas_needed = len(gas)-1, 0
-        
-        for i in range(len(gas)-1,-1,-1):
-            if gas[i]-cost[i] >= max(0,gas_needed): ans = i
-            gas_needed += cost[i]-gas[i]
-            gas_needed = max(gas_needed, cost[i]-gas[i])
-            
+        start = 0
         tank = 0
+
+        for i,g,c in zip(range(len(gas)),gas,cost):
+            tank += g-c
+
+            if tank < 0:
+                tank = 0
+                start = i+1
+
+        tank = 0
+
         for i in range(len(gas)):
-            ind = (ans+i)%len(gas)
-            tank += gas[ind]-cost[ind]
+            tank += gas[(start+i)%len(gas)]-cost[(start+i)%len(cost)]
             if tank < 0: return -1
-        
-        return ans
+
+        return start
