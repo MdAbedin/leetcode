9595e78b0db00be45dd8d1c7059959e3231ee493
Md Abedin <mdabedincs@gmail.com>
2023-10-07 22:27:34 -0400

    update 1458

--- 1401 - 1500/1458 Max Dot Product of Two Subsequences.py
+++ 1401 - 1500/1458 Max Dot Product of Two Subsequences.py
@@ -1,19 +1,6 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        ans = nums1[0]*nums2[0]
-        dp = [[0]*len(nums2) for e in nums1]
-        dp[0][0] = ans
-        
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                possibles = []
-                if i-1>=0 and j-1>= 0: possibles.append(dp[i-1][j-1])
-                if i-1>=0 and j-1>= 0: possibles.append(nums1[i]*nums2[j] + max(0, dp[i-1][j-1]))                
-                if i-1>=0: possibles.append(dp[i-1][j])
-                if j-1>=0: possibles.append(dp[i][j-1])
-                possibles.append(nums1[i]*nums2[j])
-                
-                dp[i][j] = max(possibles)
-                ans = max(ans,dp[i][j])
-                    
-        return ans
+        @cache
+        def solve(i1,i2): return 0 if i1 == len(nums1) or i2 == len(nums2) else max(nums1[i1]*nums2[i2] + solve(i1+1,i2+1),solve(i1+1,i2),solve(i1,i2+1))
+
+        return max(nums1[i1]*nums2[i2] + solve(i1+1,i2+1) for i1 in range(len(nums1)) for i2 in range(len(nums2)))
