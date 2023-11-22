65db7bf4fda5c9a34c7ad1c39e8678a44a62ce14
Md Abedin <mdabedincs@gmail.com>
2023-11-21 20:24:02 -0500

    update 1424

--- 1401 - 1500/1424 Diagonal Traverse II.py
+++ 1401 - 1500/1424 Diagonal Traverse II.py
@@ -1,14 +1,3 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        diags = defaultdict(deque)
-        
-        for i in range(len(nums)):
-            for j in range(len(nums[i])):
-                diags[i+j].appendleft(nums[i][j])
-        
-        ans = []
-        for key in sorted(diags.keys()):
-            for n in diags[key]:
-                ans.append(n)
-                
-        return ans
+        return [num for x1,x2,num in sorted([r+c,-r,nums[r][c]] for r in range(len(nums)) for c in range(len(nums[r])))]
