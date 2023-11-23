38b632c7d07c11ae8877506861592512defa73d5
Md Abedin <mdabedincs@gmail.com>
2023-11-22 19:04:55 -0500

    update 1630

--- 1601 - 1700/1630 Arithmetic Subarrays.py
+++ 1601 - 1700/1630 Arithmetic Subarrays.py
@@ -1,12 +1,3 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        ans = []
-
-        for left,right in zip(l,r):
-            subarray_set = set(nums[left:right+1])
-            min_num, max_num = min(subarray_set), max(subarray_set)
-            d = (max_num - min_num) / (right - left)
-
-            ans.append(all(min_num + i*d in subarray_set for i in range(right - left)))
-
-        return ans
+        return [len({num2-num1 for num1,num2 in pairwise(sorted(nums[left:right+1]))}) == 1 for left,right in zip(l,r)]
