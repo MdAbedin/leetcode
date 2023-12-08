e2e31b43c1682f79eec82404d4afadb975a1c1b0
Md Abedin <mdabedincs@gmail.com>
2023-12-08 10:44:02 -0500

    update 0187

--- 0101 - 0200/0187 Repeated DNA Sequences.py
+++ 0101 - 0200/0187 Repeated DNA Sequences.py
@@ -1,17 +1,3 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        if len(s) < 10:
-            return []
-        
-        seq = list(s[:10])
-        counts = defaultdict(int)
-        counts[''.join(seq)] = 1
-        
-        for i in range(10,len(s)):
-            for j in range(9):
-                seq[j] = seq[j+1]
-                
-            seq[-1] = s[i]
-            counts[''.join(seq)] += 1
-            
-        return [seq for seq in counts if counts[seq] >= 2]
+        return [s2 for s2,c in Counter(s[i-9:i+1] for i in range(9,len(s))).items() if c >= 2]
