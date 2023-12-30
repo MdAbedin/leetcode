cca12908de7d9f2df2d6a5a9f9f573e1e15afcd0
Md Abedin <mdabedincs@gmail.com>
2023-12-30 07:50:01 -0500

    update 0222

--- 0201 - 0300/0222 Count Complete Tree Nodes.py
+++ 0201 - 0300/0222 Count Complete Tree Nodes.py
@@ -1,38 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def is_at_least_size(self, root, size, height):
-        while root and height > 1:
-            if size%(2**(height-1)) >= 2**(height-2):
-                root = root.right
-            else:
-                root = root.left
-                
-            height -= 1
-            
-        return root is not None
-    
-    def countNodes(self, root: TreeNode) -> int:
-        height = 0
-        cur = root
-        while cur:
-            cur = cur.left
-            height += 1
-            
-        l,r = 2**(height-1), 2**height-1
-        ans = 0
-        
-        while l <= r:
-            mid = (l+r)//2
-            
-            if self.is_at_least_size(root, mid, height):
-                ans = mid
-                l = mid+1
-            else:
-                r = mid-1
-                
-        return ans
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def not_exists(num):
+            node = root
+
+            for bit in bin(num)[3:]:
+                if not node: break
+                if bit == "0": node = node.left
+                else: node = node.right
+
+            return not bool(node)
+
+        return bisect_left(range(1,5*10**4+1),True,key=not_exists)
