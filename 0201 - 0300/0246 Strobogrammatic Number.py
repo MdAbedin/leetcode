ce0201e6656b4c7663c93831d5a44609205bba77
Md Abedin <mdabedincs@gmail.com>
2024-01-03 14:13:39 -0500

    update 0246

--- 0201 - 0300/0246 Strobogrammatic Number.py
+++ 0201 - 0300/0246 Strobogrammatic Number.py
@@ -1,9 +1,3 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        maps = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
-        
-        for i in range(len(num)):
-            if not (num[i] in maps and maps[num[i]] == num[-1*i-1]):
-                return False
-            
-        return True
+        return all({"0":"0","1":"1","6":"9","8":"8","9":"6"}.get(d1) == d2 for d1,d2 in zip(str(num),str(num)[::-1]))
