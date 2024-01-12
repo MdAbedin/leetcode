1e2d03bfeb7a7c831fd0d040d50dc38e85742f2c
Md Abedin <mdabedincs@gmail.com>
2024-01-11 21:41:32 -0500

    update 1704

--- 1701 - 1800/1704 Determine if String Halves Are Alike.py
+++ 1701 - 1800/1704 Determine if String Halves Are Alike.py
@@ -1,7 +1,3 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        a,b = s[:len(s)//2], s[len(s)//2:]
-        a = [c.lower() for c in a]
-        b = [c.lower() for c in b]
-        
-        return a.count('a')+a.count('e')+a.count('i')+a.count('o')+a.count('u')==b.count('a')+b.count('e')+b.count('i')+b.count('o')+b.count('u')
+        return sum(c in "aeiou" for c in s[:len(s)//2].lower()) == sum(c in "aeiou" for c in s[len(s)//2:].lower())
