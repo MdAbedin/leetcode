d8d0bde01ef5f673051640408067c94c094a8d0e
Md Abedin <mdabedincs@gmail.com>
2024-01-17 19:16:49 -0500

    update 0070

--- 0001 - 0100/0070 Climbing Stairs.py
+++ 0001 - 0100/0070 Climbing Stairs.py
@@ -1,12 +1,5 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-      if n <= 2: return n
-      
-      dp = [0]*(n)
-      dp[0] = 1
-      dp[1] = 2
-      
-      for i in range(2, n):
-        dp[i] = dp[i-1]+dp[i-2]
-        
-      return dp[-1]
+        a,b = 0,1
+        for i in range(n): a,b = b,a+b
+        return b
