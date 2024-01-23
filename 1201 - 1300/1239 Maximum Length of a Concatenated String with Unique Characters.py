766cce7e6c043424ef4ed29b665936b29a03e7f9
Md Abedin <mdabedincs@gmail.com>
2024-01-22 19:45:47 -0500

    update 1239

--- 1201 - 1300/1239 Maximum Length of a Concatenated String with Unique Characters.py
+++ 1201 - 1300/1239 Maximum Length of a Concatenated String with Unique Characters.py
@@ -1,16 +1,3 @@
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        ans = 0
-        
-        for subset in powerset(arr):
-            total_len = sum(len(string) for string in subset)
-            num_chars = len(set(chain.from_iterable(subset)))
-            
-            ans = max(ans, total_len if total_len == num_chars else 0)
-            
-        return ans
+        return max(len("".join(subset)) for l in range(len(arr)+1) for subset in combinations(arr,l) if len(set("".join(subset))) == len("".join(subset)))
