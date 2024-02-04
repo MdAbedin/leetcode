fd08c9269887c347caf1c2115693fa52242b2cc3
Md Abedin <mdabedincs@gmail.com>
2024-02-03 22:06:28 -0500

    update 0076

--- 0001 - 0100/0076 Minimum Window Substring.py
+++ 0001 - 0100/0076 Minimum Window Substring.py
@@ -1,33 +1,15 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t:
-            quit()
-        if not s:
-            return ""
-        
-        left = ans_left = 0
-        right = -1
-        ans_right = len(s)-1
-        to_find = 0
-        char_counts = defaultdict(int)
-        
-        for char in t:
-            char_counts[char] += 1
-            to_find += 1
-        
-        while right+1 < len(s):
-            right += 1
-            if s[right] in char_counts:
-                to_find -= 1 if char_counts[s[right]] >= 1 else 0
-                char_counts[s[right]] -= 1
-            
-            while to_find == 0 and (s[left] not in char_counts or char_counts[s[left]] < 0):
-                if s[left] in char_counts:
-                    char_counts[s[left]] += 1
-                left += 1
-                
-            if to_find == 0 and right-left < ans_right-ans_left:
-                ans_right = right
-                ans_left = left
-                
-        return s[ans_left:ans_right+1] if to_find == 0 else ""
+        ct = Counter(t)
+
+        def solve(l):
+            c = Counter()
+
+            for i in range(len(s)):
+                c[s[i]] += 1
+                if all(c[ch] >= count for ch,count in ct.items()): return True,s[i-l+1:i+1]
+                if i-l+1 >= 0: c[s[i-l+1]] -= 1
+
+            return False,""
+
+        return solve(bisect_left(range(1,len(s)+1),(True,""),key=solve)+1)[1]
