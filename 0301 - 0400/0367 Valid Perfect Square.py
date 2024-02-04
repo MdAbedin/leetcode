f99fcf0e9b82c1f6b44311ba7367b3989627d324
Md Abedin <mdabedincs@gmail.com>
2024-02-04 14:21:39 -0500

    update 0367

--- 0301 - 0400/0367 Valid Perfect Square.py
+++ 0301 - 0400/0367 Valid Perfect Square.py
@@ -1,16 +1,3 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        l,r = 1,ceil(num/2)
-        
-        while l <= r:
-            mid = (l+r)//2
-            square = mid**2
-            
-            if square < num:
-                l = mid+1
-            elif square > num:
-                r = mid-1
-            else:
-                return True
-            
-        return False
+        return (bisect_left(range(num+1),num,key=lambda x: x**2))**2 == num
