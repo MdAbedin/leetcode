68cb8ed2b04c9fa608a95af50788ccf70661d3f7
Md Abedin <mdabedincs@gmail.com>
2024-02-06 21:54:02 -0500

    update 0451

--- 0401 - 0500/0451 Sort Characters By Frequency.py
+++ 0401 - 0500/0451 Sort Characters By Frequency.py
@@ -1,15 +1,4 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         counts = Counter(s)
-        chars = defaultdict(list)
-        
-        for char in counts:
-            chars[counts[char]].append(char)
-            
-        ans = ''
-        
-        for count in sorted(chars.keys(),reverse=True):
-            for char in chars[count]:
-                ans += char*count
-                
-        return ans
+        return "".join(sorted(s,key=lambda c: [-counts[c],c]))
