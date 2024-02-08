5173fcbb25b312bc707735061559730bf9c51f43
Md Abedin <mdabedincs@gmail.com>
2024-02-08 00:11:03 -0500

    update 0279

--- 0201 - 0300/0279 Perfect Squares.py
+++ 0201 - 0300/0279 Perfect Squares.py
@@ -1,10 +1,8 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [float('inf')]*(n+1)
-        dp[0] = 0
-        
-        for i in range(1,n+1):
-          for j in range(1, int(sqrt(i)+1)):
-            dp[i] = min(dp[i], dp[i-j**2]+1)
-            
-        return dp[-1]
+        squares = [x**2 for x in range(1,int(sqrt(n))+1)]
+
+        @cache
+        def solve(x): return 0 if x == 0 else min([1+solve(x-s) for s in squares if s <= x],default=inf)
+
+        return solve(n)
