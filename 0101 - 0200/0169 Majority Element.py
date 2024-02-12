c2416b5f563766ac23e97f381adcc16ecb7daa4e
Md Abedin <mdabedincs@gmail.com>
2024-02-12 09:24:07 -0500

    update 0169

--- 0101 - 0200/0169 Majority Element.py
+++ 0101 - 0200/0169 Majority Element.py
@@ -1,5 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = Counter(nums)
-        
-        return reduce(lambda a,b: b if counts[b] > len(nums)//2 else a, counts, 0)
+        return max(c := Counter(nums),key = c.__getitem__)
