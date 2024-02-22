f01d056ed1305ae9bff27a6300514ac5618b1a28
Md Abedin <mdabedincs@gmail.com>
2024-02-21 21:09:39 -0500

    update 0997

--- 0901 - 1000/0997 Find the Town Judge.py
+++ 0901 - 1000/0997 Find the Town Judge.py
@@ -1,11 +1,11 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_counts = defaultdict(lambda: [0,0])
-        
-        for truster, trusted in trust:
-            trust_counts[truster][0] += 1
-            trust_counts[trusted][1] += 1
-            
-        candidates = [person for person in range(1,n+1) if trust_counts[person] == [0,n-1]]
-        
-        return candidates[0] if len(candidates) == 1 else -1
+        ins,outs = [0]*n,[0]*n
+
+        for a,b in trust:
+            ins[b-1] += 1
+            outs[a-1] += 1
+
+        judges = [i for i in range(n) if ins[i] == n-1 and outs[i] == 0]
+
+        return -1 if len(judges) != 1 else judges[0]+1
