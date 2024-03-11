46f7c29eeef7485002dca9555456f3c02a977667
Md Abedin <mdabedincs@gmail.com>
2024-03-10 20:05:53 -0400

    update 0791

--- 0701 - 0800/0791 Custom Sort String.py
+++ 0701 - 0800/0791 Custom Sort String.py
@@ -1,16 +1,3 @@
 class Solution:
-    def customSortString(self, S: str, T: str) -> str:
-        ans = ""
-        
-        char_counts = defaultdict(int)
-        for char in T:
-            char_counts[char] += 1
-            
-        for char in S:
-            ans += char_counts[char]*char
-            char_counts[char] = 0
-            
-        for char in char_counts:
-            ans += char_counts[char]*char
-            
-        return ans
+    def customSortString(self, order: str, s: str) -> str:
+        return "".join(sorted(s,key = lambda c: order.find(c)))
