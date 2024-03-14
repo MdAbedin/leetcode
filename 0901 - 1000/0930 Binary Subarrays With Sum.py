0f936ad371b1b5e6bf53a3440049eb23f074e61d
Md Abedin <mdabedincs@gmail.com>
2024-03-13 20:08:22 -0400

    update 0930

--- 0901 - 1000/0930 Binary Subarrays With Sum.py
+++ 0901 - 1000/0930 Binary Subarrays With Sum.py
@@ -1,12 +1,11 @@
 class Solution:
-    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
-        sum_counts = defaultdict(int)
-        sum_counts[0] = 1
-        ans = cur_sum = 0
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        counts = Counter({0:1})
+        ps = ans = 0
 
-        for bit in A:
-            cur_sum += bit
-            ans += sum_counts[cur_sum-S]
-            sum_counts[cur_sum] += 1
-            
+        for num in nums:
+            ps += num
+            ans += counts[ps-goal]
+            counts[ps] += 1
+
         return ans
