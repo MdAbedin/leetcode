f7f421b9a1379b44f4619a04f4d0a9904af4da13
Md Abedin <mdabedincs@gmail.com>
2024-03-14 21:38:35 -0400

    update 0238

--- 0201 - 0300/0238 Product of Array Except Self.py
+++ 0201 - 0300/0238 Product of Array Except Self.py
@@ -1,15 +1,3 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        prods = [1]*len(nums)
-        
-        for i in range(len(nums)):
-            prods[i] = prod
-            prod *= nums[i]
-            
-        prod = 1
-        for i in range(len(nums)-1, -1, -1):
-            prods[i] *= prod
-            prod *= nums[i]
-            
-        return prods
+        return [l*r for l,r in zip([1]+list(accumulate(nums[:-1],mul)), list(accumulate(nums[:0:-1],mul))[::-1]+[1])]
