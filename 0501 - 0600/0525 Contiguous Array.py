c19267d4f124123eaf20c94d644553c7670b1c8b
Md Abedin <mdabedincs@gmail.com>
2024-03-15 21:37:36 -0400

    update 0525

--- 0501 - 0600/0525 Contiguous Array.py
+++ 0501 - 0600/0525 Contiguous Array.py
@@ -1,14 +1,11 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        ssum_locs = {0: -1}
-        ans = 0
-        ssum = 0
-        
-        for i in range(len(nums)):
-            ssum += 1 if nums[i] == 1 else -1
-            if ssum in ssum_locs:
-                ans = max(ans, i - ssum_locs[ssum])
-            else:
-                ssum_locs[ssum] = i
-                
+        first_inds = {0:-1}
+        ans = ps = 0
+
+        for i,num in enumerate(nums):
+            ps += 1 if num == 1 else -1
+            if ps in first_inds: ans = max(ans,i-first_inds[ps])
+            else: first_inds[ps] = i
+
         return ans
