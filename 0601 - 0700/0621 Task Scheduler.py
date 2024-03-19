dff93d5e3ef07df9d454d958d6e5565b26896a17
Md Abedin <mdabedincs@gmail.com>
2024-03-18 21:45:35 -0400

    update 0621

--- 0601 - 0700/0621 Task Scheduler.py
+++ 0601 - 0700/0621 Task Scheduler.py
@@ -1,10 +1,3 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        if n == 0: return len(tasks)
-        
-        counts = Counter(tasks)
-        max_count = max(counts[c] for c in counts)
-        num_max = sum(1 if counts[c] == max_count else 0 for c in counts)
-        
-        if (max_count-1)*(n+1)+1+num_max-1 >= len(tasks): return (max_count-1)*(n+1)+1+num_max-1
-        return len(tasks)
+        return max(len(tasks),(max(Counter(tasks).values())-1)*(n+1)+list(Counter(tasks).values()).count(max(Counter(tasks).values())))
