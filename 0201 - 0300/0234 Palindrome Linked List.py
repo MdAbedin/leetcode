e18d7730f13e2d0f463916ab7a61af3df3954f5e
Md Abedin <mdabedincs@gmail.com>
2024-03-21 20:14:07 -0400

    update 0234

--- 0201 - 0300/0234 Palindrome Linked List.py
+++ 0201 - 0300/0234 Palindrome Linked List.py
@@ -1,3 +1,3 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        return (nums := [node.val for node in takewhile(lambda x: x, accumulate(repeat(head),lambda x,_: x.next))]) == nums[::-1]
+        return (nums := [node.val for node in takewhile(lambda x: x,accumulate(repeat(head),lambda x,_: x.next))]) == nums[::-1]
