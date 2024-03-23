65f3d8ae6b8eb277acf3444a28cd425fc3feac7f
Md Abedin <mdabedincs@gmail.com>
2024-03-22 21:29:45 -0400

    update 0143

--- 0101 - 0200/0143 Reorder List.py
+++ 0101 - 0200/0143 Reorder List.py
@@ -1,30 +1,26 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head: return None
-
         length = 0
         cur = head
 
         while cur:
             length += 1
+            tail = cur
             cur = cur.next
 
         cur = head
         for i in range(length//2): cur = cur.next
-        nxt = cur.next
+        prev = None
 
-        while nxt:
-            after = nxt.next
-            nxt.next = cur
-            cur,nxt = nxt,after
+        while cur:
+            next_,cur.next,prev = cur.next,prev,cur
+            cur = next_
 
-        cur1,cur2 = head,cur
+        cur1,cur2 = head,tail
 
         for i in range((length-1)//2):
             cur1_next,cur2_next = cur1.next,cur2.next
             cur1.next,cur2.next = cur2,cur1_next
             cur1,cur2 = cur1_next,cur2_next
-
-        cur2.next = None
 
         return head
