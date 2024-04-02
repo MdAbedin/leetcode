6bb75d502889190c1e250e9544c642f6babd0597
Md Abedin <mdabedincs@gmail.com>
2024-04-01 20:39:16 -0400

    update 0205

--- 0201 - 0300/0205 Isomorphic Strings.py
+++ 0201 - 0300/0205 Isomorphic Strings.py
@@ -1,8 +1,4 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         m1,m2 = {},{}
-
-        for c1,c2 in zip(s,t):
-            if m1.setdefault(c1,c2) != c2 or m2.setdefault(c2,c1) != c1: return False
-
-        return True
+        return all(m1.setdefault(c1,c2) == c2 and m2.setdefault(c2,c1) == c1 for c1,c2 in zip(s,t))
