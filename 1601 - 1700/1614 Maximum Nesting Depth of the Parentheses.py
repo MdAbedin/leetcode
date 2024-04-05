27ff0bb889e9e8c53e8b5fece3c11902417f4102
Md Abedin <mdabedincs@gmail.com>
2024-04-04 22:56:32 -0400

    update 1614

--- 1601 - 1700/1614 Maximum Nesting Depth of the Parentheses.py
+++ 1601 - 1700/1614 Maximum Nesting Depth of the Parentheses.py
@@ -1,11 +1,3 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        d = 0
-        ans = 0
-
-        for c in s:
-            if c == "(": d += 1
-            elif c == ")": d -= 1
-            ans = max(ans,d)
-
-        return ans
+        return max(accumulate((c=="(")-(c==")") for c in s))
