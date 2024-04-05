1ef48414f8402267d213ccfb81cb6b2bb4fea463
Md Abedin <mdabedincs@gmail.com>
2024-04-04 23:39:21 -0400

    update 1544

--- 1501 - 1600/1544 Make The String Great.py
+++ 1501 - 1600/1544 Make The String Great.py
@@ -3,7 +3,7 @@ class Solution:
         stack = []
 
         for c in s:
-            if stack and c.isalpha() and stack[-1] == c.swapcase(): stack.pop()
+            if stack and stack[-1] == c.swapcase(): stack.pop()
             else: stack.append(c)
 
         return "".join(stack)
