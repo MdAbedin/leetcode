f3147fd13b2ebf14742c002fe51cce27118c91ca
Md Abedin <mdabedincs@gmail.com>
2024-04-07 20:16:18 -0400

    update 1700

--- 1601 - 1700/1700 Number of Students Unable to Eat Lunch.py
+++ 1601 - 1700/1700 Number of Students Unable to Eat Lunch.py
@@ -1,13 +1,9 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        sandwiches = deque(sandwiches)
-        students = deque(students)
-        
-        while not (all(a!=sandwiches[0] for a in students)):
-            if students[0] == sandwiches[0]:
-                students.popleft()
-                sandwiches.popleft()
-            else:
-                students.append(students.popleft())
-                
-        return len(students)
+        counts = Counter(students)
+
+        for s in sandwiches:
+            if counts[s] == 0: return counts[1-s]
+            counts[s] -= 1
+
+        return 0
