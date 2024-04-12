bcff228403499b1cbde1c70e2fe5e2d89a76497c
Md Abedin <mdabedincs@gmail.com>
2024-04-11 22:58:13 -0500

    update 0042

--- 0001 - 0100/0042 Trapping Rain Water.py
+++ 0001 - 0100/0042 Trapping Rain Water.py
@@ -1,12 +1,10 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        ls, rs = [], []
-        for i in range(len(height)):
-            ls.append(max(height[i], ls[-1] if ls else -1))
-            rs.append(max(height[-1*i-1], rs[-1] if rs else -1))
-        rs = rs[::-1]
-        ans = 0
-        
-        for i in range(len(height)):
-            ans += min(ls[i], rs[i])-height[i]
-        return ans
+        l = []
+        for h in height: l.append(max(0 if not l else l[-1],h))
+
+        r = []
+        for h in height[::-1]: r.append(max(0 if not r else r[-1],h))
+        r.reverse()
+
+        return sum(min(l,r)-h for l,r,h in zip(l,r,height))
