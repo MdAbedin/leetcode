136b8fd3d0882b88fe24ba604693c186f3f361bf
Md Abedin <mdabedincs@gmail.com>
2024-04-14 20:06:25 -0400

    update 0129

--- 0101 - 0200/0129 Sum Root to Leaf Numbers.py
+++ 0101 - 0200/0129 Sum Root to Leaf Numbers.py
@@ -1,7 +1,5 @@
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode], parent_num = 0) -> int:
+    def sumNumbers(self, root: Optional[TreeNode],parent_num=0) -> int:
         if not root: return 0
-        
-        num = parent_num*10 + root.val
-        
-        return num if not root.left and not root.right else self.sumNumbers(root.left, num) + self.sumNumbers(root.right, num)
+        num = parent_num*10+root.val
+        return num if not root.left and not root.right else self.sumNumbers(root.left,num)+self.sumNumbers(root.right,num)
