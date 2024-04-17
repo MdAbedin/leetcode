490cc40f12788be02729d49d3a6a8534515fa3bc
Md Abedin <mdabedincs@gmail.com>
2024-04-16 21:05:06 -0400

    update 0988

--- 0901 - 1000/0988 Smallest String Starting From Leaf.py
+++ 0901 - 1000/0988 Smallest String Starting From Leaf.py
@@ -1,39 +1,8 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        dfs = [root]
-        bfs = deque()
-        root.parent = None
+        def solve(node,s):
+            if not node: return "{"
+            s = chr(ord("a")+node.val)+s
+            return s if not node.left and not node.right else min(solve(node.left,s),solve(node.right,s))
 
-        while dfs:
-            cur = dfs.pop()
-
-            if not cur.left and not cur.right: bfs.append(cur)
-
-            if cur.left:
-                cur.left.parent = cur
-                dfs.append(cur.left)
-            
-            if cur.right:
-                cur.right.parent = cur
-                dfs.append(cur.right)
-
-        ans = []
-
-        while bfs:
-            min_char = min(node.val for node in bfs)
-            ans.append(min_char)
-
-            for i in range(len(bfs)):
-                cur = bfs.popleft()
-                
-                if cur.val == min_char:
-                    if cur.parent:
-                        bfs.append(cur.parent)
-                    else:
-                        return "".join(chr(ord('a') + val) for val in ans)
+        return solve(root,"")
