acafd1c2db768b2cbd923b85b696ab37573f6660
Md Abedin <mdabedincs@gmail.com>
2024-04-18 22:33:09 -0400

    update 0200

--- 0101 - 0200/0200 Number of Islands.py
+++ 0101 - 0200/0200 Number of Islands.py
@@ -1,26 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        R,C = len(grid),len(grid[0])
         ans = 0
-        
-        for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if grid[y][x] == '1':
-                    ans += 1
-                    
-                    bfs = deque()
-                    bfs.append((y,x))
-                    
-                    while bfs:
-                        cur_y, cur_x = bfs.popleft()
-                        
-                        grid[cur_y][cur_x] = '-1'
-                        
-                        for y_shift, x_shift in ((1,0), (0,1), (-1,0), (0,-1)):
-                            new_y = cur_y + y_shift
-                            new_x = cur_x + x_shift
-                            
-                            if new_y >= 0 and new_y < len(grid) and new_x >= 0 and new_x < len(grid[0]) and grid[new_y][new_x] == '1':
-                                grid[new_y][new_x] = '-1'
-                                bfs.append((new_y, new_x))
-                                
+
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] != "1": continue
+
+                ans += 1
+                bfs = [(r,c)]
+
+                for r2,c2 in bfs:
+                    for r3,c3 in [[r2+1,c2],[r2-1,c2],[r2,c2+1],[r2,c2-1]]:
+                        if r3 not in range(R) or c3 not in range(C) or grid[r3][c3] != "1": continue
+                        grid[r3][c3] = "2"
+                        bfs.append((r3,c3))
+
         return ans
