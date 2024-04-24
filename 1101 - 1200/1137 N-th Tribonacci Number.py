6a4be76edc1527cb1b4eaa22e72c553cb6346052
Md Abedin <mdabedincs@gmail.com>
2024-04-23 22:53:24 -0400

    update 1137

--- 1101 - 1200/1137 N-th Tribonacci Number.py
+++ 1101 - 1200/1137 N-th Tribonacci Number.py
@@ -1,9 +1,5 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n <= 1: return n
-        cur,prev,prev2 = 1,1,0
-        
-        for i in range(3,n+1):
-            cur,prev,prev2 = cur+prev+prev2,cur,prev
-        
-        return cur
+        nums = [1,0,0]
+        for _ in range(n): nums = nums[1:]+[sum(nums)]
+        return nums[-1]
