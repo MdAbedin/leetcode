7cbc492b653d54fda302edfed4d11280921c7624
Md Abedin <mdabedincs@gmail.com>
2024-05-16 16:35:55 -0400

    update 2331

--- 2301 - 2400/2331 Evaluate Boolean Binary Tree.py
+++ 2301 - 2400/2331 Evaluate Boolean Binary Tree.py
@@ -1,2 +1,2 @@
 class Solution:
-    def evaluateTree(self,root: Optional[TreeNode]) -> bool:return(f:=lambda x:x and[ne,eq,or_,and_][x.val](*map(f,[x.left,x.right])))(root)
+    def evaluateTree(self,root: Optional[TreeNode]) -> bool:return(f:=lambda x:x and[ne,eq,or_,and_][x.val](f(x.left),f(x.right)))(root)
