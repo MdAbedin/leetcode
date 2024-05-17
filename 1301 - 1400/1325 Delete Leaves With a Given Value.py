643cfa5db1c3368f95c339cdb33181ad764d9b2c
Md Abedin <mdabedincs@gmail.com>
2024-05-16 20:26:02 -0400

    add 1325

---
+++ 1301 - 1400/1325 Delete Leaves With a Given Value.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if not root: return None
+
+        root.left = self.removeLeafNodes(root.left,target)
+        root.right = self.removeLeafNodes(root.right,target)
+
+        return None if not root.left and not root.right and root.val == target else root
