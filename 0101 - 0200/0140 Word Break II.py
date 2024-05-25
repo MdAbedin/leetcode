7e93ac847fa6d4de3246e75f264e342df9517b4d
Md Abedin <mdabedincs@gmail.com>
2024-05-24 20:52:28 -0400

    update 0140

--- 0101 - 0200/0140 Word Break II.py
+++ 0101 - 0200/0140 Word Break II.py
@@ -1,9 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         @cache
-        def solve(i):
-            if i == len(s): return [""]
-            if i > len(s): return []
-            return [(w + " " + ans).rstrip() for w in wordDict for ans in solve(i+len(w)) if s.startswith(w,i)]
-
-        return solve(0)
+        def solve(i): return [[]] if i == len(s) else [[word,*ans] for word in wordDict if s.startswith(word,i) for ans in solve(i+len(word))]
+        return [" ".join(words) for words in solve(0)]
