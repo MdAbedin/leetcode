11716217e22ccbd6cb2aa9e149308f94c6e8f313
Md Abedin <mdabedincs@gmail.com>
2024-05-30 22:21:32 -0400

    update 0260

--- 0201 - 0300/0260 Single Number III.py
+++ 0201 - 0300/0260 Single Number III.py
@@ -1,12 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        xor_all = reduce(lambda a,b: a^b, nums)
+        xor_all = reduce(xor,nums)
         b = xor_all
         
         for num in nums:
-            num2 = xor_all^num
-            
-            if num < num2:
-                b ^= num
+            if num < xor_all^num: b ^= num
                 
-        return [xor_all^b, b]
+        return [xor_all^b,b]
