59760d88bf765d062e4e66b391e93320ae5cec0a
Md Abedin <mdabedincs@gmail.com>
2024-05-30 23:12:04 -0400

    update 1442

--- 1401 - 1500/1442 Count Triplets That Can Form Two Arrays of Equal XOR.py
+++ 1401 - 1500/1442 Count Triplets That Can Form Two Arrays of Equal XOR.py
@@ -2,11 +2,12 @@ class Solution:
     def countTriplets(self, arr: List[int]) -> int:
         ans = 0
         xor = 0
-        inds = defaultdict(list,{0:[-1]})
+        data = defaultdict(lambda:[0,0],{0:[0,1]})
 
         for i,x in enumerate(arr):
             xor ^= x
-            ans += sum(i-i2-1 for i2 in inds[xor])
-            inds[xor].append(i)
+            ans += i*data[xor][1]-data[xor][0]
+            data[xor][0] += i+1
+            data[xor][1] += 1
 
         return ans
