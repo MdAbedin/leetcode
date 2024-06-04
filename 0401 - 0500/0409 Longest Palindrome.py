145fc5f0273e8acc08596fd8ee56af2df9d0da0e
Md Abedin <mdabedincs@gmail.com>
2024-06-03 23:05:32 -0400

    update 0409

--- 0401 - 0500/0409 Longest Palindrome.py
+++ 0401 - 0500/0409 Longest Palindrome.py
@@ -1,4 +1,3 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counts = Counter(s)
-        return sum(c//2 for c in counts.values())*2 + any(c%2 == 1 for c in counts.values())
+        return sum(c-c%2 for c in Counter(s).values())+any(c%2 == 1 for c in Counter(s).values())
