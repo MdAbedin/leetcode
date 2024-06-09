d5033083eae0fdd4ba6e560b11b0ab281ad504b8
Md Abedin <mdabedincs@gmail.com>
2024-06-08 20:39:59 -0400

    update 0974

--- 0901 - 1000/0974 Subarray Sums Divisible by K.py
+++ 0901 - 1000/0974 Subarray Sums Divisible by K.py
@@ -1,15 +1,12 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        prefix_sum_mod_counts = Counter({0:1})
-        prefix_sum_mod = 0
+        counts = Counter({0:1})
+        ps = 0
         ans = 0
-        
+
         for num in nums:
-            prefix_sum_mod += num
-            prefix_sum_mod %= k
-            
-            ans += prefix_sum_mod_counts[prefix_sum_mod]
-            
-            prefix_sum_mod_counts[prefix_sum_mod] += 1
-        
+            ps += num
+            ans += counts[ps%k]
+            counts[ps%k] += 1
+
         return ans
