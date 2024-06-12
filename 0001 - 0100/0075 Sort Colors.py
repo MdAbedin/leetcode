feafd0dc3172b80a8c30d627ad92b803a844f11f
Md Abedin <mdabedincs@gmail.com>
2024-06-11 20:09:47 -0400

    update 0075

--- 0001 - 0100/0075 Sort Colors.py
+++ 0001 - 0100/0075 Sort Colors.py
@@ -3,11 +3,10 @@ class Solution:
         i0,i1 = 0,0
 
         for i in range(len(nums)):
-            i1 = max(i1,i0)
-            
             if nums[i] == 0:
                 nums[i0],nums[i] = nums[i],nums[i0]
                 i0 += 1
+                i1 = max(i1,i0)
 
             if nums[i] == 1:
                 nums[i1],nums[i] = nums[i],nums[i1]
