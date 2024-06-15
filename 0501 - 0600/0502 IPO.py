3c3fc93e97d629eff4301d6fa4aa7ae8da8d46c7
Md Abedin <mdabedincs@gmail.com>
2024-06-14 20:17:22 -0400

    update 0502

--- 0501 - 0600/0502 IPO.py
+++ 0501 - 0600/0502 IPO.py
@@ -1,14 +1,10 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        projects = deque(sorted([[c,p] for p,c in zip(profits,capital)]))
+        projects = sorted(zip(map(neg,profits),capital),key=lambda p: -p[1])
         pq = []
-        capital = w
         
         for i in range(k):
-            while projects and projects[0][0] <= capital:
-                c,p = projects.popleft()
-                heappush(pq, [-p,c])
-                
-            if pq: capital += -heappop(pq)[0]
+            while projects and projects[-1][1] <= w: heappush(pq, projects.pop())
+            if pq: w += -heappop(pq)[0]
         
-        return capital
+        return w
