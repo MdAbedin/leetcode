b4710fe364bcc59db379b86d3f50a6f4cb38f8a4
Md Abedin <mdabedincs@gmail.com>
2024-06-18 23:01:40 -0400

    update 1482

--- 1401 - 1500/1482 Minimum Number of Days to Make m Bouquets.py
+++ 1401 - 1500/1482 Minimum Number of Days to Make m Bouquets.py
@@ -1,19 +1,3 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        def works(d):
-            b = 0
-            s = 0
-
-            for bd in bloomDay:
-                if d >= bd: s += 1
-                else: s = 0
-
-                if s >= k:
-                    b += 1
-                    s = 0
-
-            return b >= m
-
-        ans = bisect_left(range(10**9+1),True,key=works)
-
-        return -1 if ans == 10**9+1 else ans
+        return -1 if (ans := bisect_left(range(10**9+1),True,key=lambda d: sum(len(list(g))//k for b,g in groupby(bloomDay,key=partial(ge,d)) if b) >= m)) == 10**9+1 else ans
