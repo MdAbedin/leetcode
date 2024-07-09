b8ea3167330c9fb6dc9ba1ae894049dc96e30f52
Md Abedin <mdabedincs@gmail.com>
2024-07-08 21:31:55 -0400

    update 1701

--- 1701 - 1800/1701 Average Waiting Time.py
+++ 1701 - 1800/1701 Average Waiting Time.py
@@ -1,10 +1,11 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        t = 1
-        s = 0
-        
-        for a,b in customers:
-            s += max(t,a)+b-a
-            t = max(t+b,a+b)
-            
-        return s/len(customers)
+        times = []
+        t = 0
+
+        for arrival,time in customers:
+            t = max(t,arrival)
+            times.append(t+time-arrival)
+            t += time
+
+        return mean(times)
