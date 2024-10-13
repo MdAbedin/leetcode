14a438aa43c0da2865137bb7e9cb74e7bf113641
Md Abedin <mdabedincs@gmail.com>
2024-10-12 20:23:29 -0400

    update 0046

--- 0001 - 0100/0046 Permutations.py
+++ 0001 - 0100/0046 Permutations.py
@@ -1,3 +1,11 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        if not nums: return [[]]
+
+        answers = []
+        
+        for i,num in enumerate(nums):
+            remaining_perms = self.permute(nums[:i] + nums[i+1:])
+            for remaining_perm in remaining_perms: answers.append([num]+remaining_perm)
+
+        return answers
