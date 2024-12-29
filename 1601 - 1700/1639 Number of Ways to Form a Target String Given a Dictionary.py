13dbd485dabbe1cf829ef0a4ca9cced0ed044601
Md Abedin <mdabedincs@gmail.com>
2024-12-28 22:07:44 -0500

    update 1639

--- 1601 - 1700/1639 Number of Ways to Form a Target String Given a Dictionary.py
+++ 1601 - 1700/1639 Number of Ways to Form a Target String Given a Dictionary.py
@@ -1,12 +1,12 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        MOD = 10**9+7
-        counters = [Counter(col) for col in zip(*words)]
+        m = 10**9+7
+        counts = list(map(Counter,zip(*words)))
         
         @cache
         def solve(i,k):
-            if len(target)-i > len(words[0])-k: return 0
             if i == len(target): return 1
-            return ((counters[k][target[i]] * solve(i+1,k+1))%MOD + solve(i,k+1))%MOD
+            if k >= len(words[0]): return 0
+            return (counts[k][target[i]]*solve(i+1,k+1)+solve(i,k+1))%m
             
         return solve(0,0)
