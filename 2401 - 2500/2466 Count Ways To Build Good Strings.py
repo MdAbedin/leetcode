37298afaedafecfb05301bf992d38dacef5eba83
Md Abedin <mdabedincs@gmail.com>
2024-12-29 22:54:34 -0500

    update 2466

--- 2401 - 2500/2466 Count Ways To Build Good Strings.py
+++ 2401 - 2500/2466 Count Ways To Build Good Strings.py
@@ -1,23 +1,11 @@
 class Solution:
-
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-
-        MOD = 10**9+7
-
-        
+        m = 10**9+7
 
         @cache
+        def solve(l):
+            if l < 0: return 0
+            if l == 0: return 1
+            return (solve(l-zero)+solve(l-one))%m
 
-        def solve(length):
-
-            if length < 0: return 0
-
-            if length == 0: return 1
-
-            
-
-            return (solve(length-zero)+solve(length-one))%MOD
-
-        
-
-        return sum(solve(length) for length in range(low,high+1))%MOD
+        return sum(solve(l) for l in range(low,high+1))%m
