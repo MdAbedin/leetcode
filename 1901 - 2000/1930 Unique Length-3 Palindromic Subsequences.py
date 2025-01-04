f37b9bc15ceb1b67843f0b8dd8d653ab7fc0d2cf
Md Abedin <mdabedincs@gmail.com>
2025-01-03 19:42:37 -0500

    update 1930

--- 1901 - 2000/1930 Unique Length-3 Palindromic Subsequences.py
+++ 1901 - 2000/1930 Unique Length-3 Palindromic Subsequences.py
@@ -1,6 +1,4 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        indexes = defaultdict(list)
-        for i,c in enumerate(s): indexes[c].append(i)
-
-        return sum(len(indexes[c1]) >= 2 and (i := bisect_right(indexes[c2],indexes[c1][0])) < len(indexes[c2]) and indexes[c2][i] < indexes[c1][-1] for c1 in ascii_lowercase for c2 in ascii_lowercase)
+        inds = {c:[s.find(c)+1,s.rfind(c)] for c in ascii_lowercase}
+        return len({c2+c+c2 for i,c in enumerate(s) for c2 in ascii_lowercase if i in range(*inds[c2])})
