79e22ede2fe3aecc7a245c9b32e6cdfe9c4a65ee
Md Abedin <mdabedincs@gmail.com>
2025-01-06 19:49:47 -0500

    update 1408

--- 1401 - 1500/1408 String Matching in an Array.py
+++ 1401 - 1500/1408 String Matching in an Array.py
@@ -1,26 +1,3 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        ans = []
-        
-        for i in range(len(words)):
-            found = False
-            
-            for j in range(len(words)):
-                if i != j:
-                    for k in range(len(words[j]) - len(words[i]) + 1):
-                        matched = True
-                        
-                        for l in range(len(words[i])):
-                            if words[j][k+l] != words[i][l]:
-                                matched = False
-                                break
-                            
-                        if matched:
-                            ans.append(words[i])
-                            found = True
-                            break
-                
-                if found:
-                    break
-                    
-        return ans
+        return [w for i,w in enumerate(words) if any(w in w2 for i2,w2 in enumerate(words) if i2 != i) ]
