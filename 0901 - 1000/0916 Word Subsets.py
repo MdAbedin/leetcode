c9c3f9a41dddd0e81b7eb24e4c7e7ae7a8c85519
Md Abedin <mdabedincs@gmail.com>
2025-01-09 19:09:42 -0500

    update 0916

--- 0901 - 1000/0916 Word Subsets.py
+++ 0901 - 1000/0916 Word Subsets.py
@@ -1,30 +1,9 @@
 class Solution:
-    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
-        ans = []
-        B_counts = defaultdict(int)
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        counts = Counter()
         
-        for word in B:
-            word_counts = defaultdict(int)
-            
-            for char in word:
-                word_counts[char] += 1
-                
-            for char in word_counts:
-                B_counts[char] = max(B_counts[char], word_counts[char])
-        
-        for word in A:
-            word_counts = defaultdict(int)
-            
-            for char in word:
-                word_counts[char] += 1
-                
-            is_universal = True
-            for char in B_counts:
-                if word_counts[char] < B_counts[char]:
-                    is_universal = False
-                    break
-                
-            if is_universal:
-                ans.append(word)
-                
-        return ans
+        for w in words2:
+            for c,count in Counter(w).items():
+                counts[c] = max(counts[c],count)
+
+        return [w for w in words1 if Counter(w) >= counts]
