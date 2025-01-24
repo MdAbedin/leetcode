9f6f526f761796b96bcb2940dc85621ef6748ac0
Md Abedin <mdabedincs@gmail.com>
2025-01-23 20:12:58 -0500

    update 0802

--- 0801 - 0900/0802 Find Eventual Safe States.py
+++ 0801 - 0900/0802 Find Eventual Safe States.py
@@ -1,20 +1,19 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        G2 = [[] for i in range(len(graph))]
+        g2 = defaultdict(list)
 
-        for i in range(len(graph)):
-            for nei in graph[i]: G2[nei].append(i)
+        for i,neis in enumerate(graph):
+            for nei in neis:
+                g2[nei].append(i)
 
-        out_degrees = {i:len(graph[i]) for i in range(len(graph))}
-        tsort = [i for i in range(len(graph)) if out_degrees[i] == 0]
+        tsort = [i for i,neis in enumerate(graph) if not neis]
         ans = []
 
-        while tsort:
-            node = tsort.pop()
+        for node in tsort:
             ans.append(node)
 
-            for nei in G2[node]:
-                out_degrees[nei] -= 1
-                if out_degrees[nei] == 0: tsort.append(nei)
+            for nei in g2[node]:
+                graph[nei].pop()
+                if not graph[nei]: tsort.append(nei)
 
         return sorted(ans)
