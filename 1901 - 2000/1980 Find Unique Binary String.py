16105328acd6545c33d955d984f062efd336c33c
Md Abedin <mdabedincs@gmail.com>
2025-02-19 19:12:48 -0500

    update 1980

--- 1901 - 2000/1980 Find Unique Binary String.py
+++ 1901 - 2000/1980 Find Unique Binary String.py
@@ -1,6 +1,3 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        nums = set(nums)
-        
-        for num in range(2**len(nums)):
-            if bin(num)[2:].rjust(len(nums),"0") not in nums: return bin(num)[2:].rjust(len(nums),"0")
+        return "".join(str(1-int(b[i])) for i,b in enumerate(nums))
