ab5bceebe10cb33240b687d2716264e91e1f6495
Md Abedin <mdabedincs@gmail.com>
2025-02-22 23:47:56 -0500

    update 0889

--- 0801 - 0900/0889 Construct Binary Tree from Preorder and Postorder Traversal.py
+++ 0801 - 0900/0889 Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -1,33 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def helper(self, pre_l, pre_r, post_l, post_r):
-        if pre_r < pre_l:
-            return None
-        if pre_r == pre_l:
-            return TreeNode(self.pre[pre_l])
-    
-        root = TreeNode(self.pre[pre_l])
-        left_val = self.pre[pre_l+1]
-        right_val = self.post[post_r-1]
-        
-        if left_val == right_val:
-            root.left = self.helper(pre_l+1, pre_r, post_l, post_r-1)
-        else:
-            pre_right_index = self.pre.index(right_val)
-            post_left_index = self.post.index(left_val)
-            
-            root.left = self.helper(pre_l+1, pre_right_index-1, post_l, post_left_index)
-            root.right = self.helper(pre_right_index, pre_r, post_left_index+1, post_r-1)
-            
-        return root
-        
-    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
-        self.pre, self.post = pre, post
-        
-        return self.helper(0, len(pre)-1, 0, len(post)-1)
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder: return None
+
+        ans = TreeNode(preorder[0])
+        counts1,counts2 = Counter(),Counter()
+
+        for i,(v1,v2) in enumerate(zip(preorder[1:],postorder[:-1])):
+            counts1[v1] += 1
+            counts2[v2] += 1
+
+            if counts1 == counts2:
+                ans.left = self.constructFromPrePost(preorder[1:i+2],postorder[:i+1])
+                ans.right = self.constructFromPrePost(preorder[i+2:],postorder[i+1:-1])
+                break
+
+        return ans
