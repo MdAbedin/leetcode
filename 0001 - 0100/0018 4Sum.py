c40e71f054f8906e36e844a84b88d704c1ef6752
Md Abedin <mdabedincs@gmail.com>
2025-02-23 23:59:36 -0500

    update 0018

--- 0001 - 0100/0018 4Sum.py
+++ 0001 - 0100/0018 4Sum.py
@@ -1,27 +1,14 @@
 class Solution:
-
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-
         counts = Counter(nums)
-
         ans = set()
-
         
-
         for i in range(len(nums)):
-
             counts[nums[i]] -= 1
-
             
-
             for j in range(i):
-
                 for k in range(j):
-
                     if counts[target-nums[i]-nums[j]-nums[k]] > 0:
-
                         ans.add(tuple(sorted([nums[i],nums[j],nums[k],target-nums[i]-nums[j]-nums[k]])))
-
                         
-
-        return ans
+        return list(ans)
