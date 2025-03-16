31d1e6f609138879f4247de82d2b26cdf2787789
Md Abedin <mdabedincs@gmail.com>
2025-03-15 21:42:06 -0400

    update 2594

--- 2501 - 2600/2594 Minimum Time to Repair Cars.py
+++ 2501 - 2600/2594 Minimum Time to Repair Cars.py
@@ -1,4 +1,3 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        counts = Counter(ranks)
-        return min(rank * (1+bisect_left(range(1,cars+1), True, key = lambda n: sum(floor(sqrt((rank*n**2)/rank2)) * counts[rank2] for rank2 in counts) >= cars))**2 for rank in counts)
+        return bisect_left(range(10**14+1),True,key = lambda t: sum(floor(sqrt(t/r)) for r in ranks) >= cars)
