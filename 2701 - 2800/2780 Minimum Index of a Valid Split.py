b4a58512996a1c768a4384f9160319a43520b0a4
Md Abedin <mdabedincs@gmail.com>
2025-03-26 23:21:30 -0400

    update 2780

--- 2701 - 2800/2780 Minimum Index of a Valid Split.py
+++ 2701 - 2800/2780 Minimum Index of a Valid Split.py
@@ -1,13 +1,11 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        c = Counter(nums)
-        d = max(c,key=c.__getitem__)
-        lc,rc = Counter(),c
-        
-        for i in range(len(nums)-1):
-            num = nums[i]
-            lc[num] += 1
-            rc[num] -= 1
-            if lc[d] > (i+1)/2 and rc[d] > (len(nums)-i-1)/2: return i
-        
+        d = sorted(nums)[len(nums)//2]
+        c1,c2 = 0,nums.count(d)
+
+        for i,num in enumerate(nums):
+            c1 += num == d
+            c2 -= num == d
+            if c1 > (i+1)//2 and c2 > (len(nums)-1-i)//2: return i
+
         return -1
