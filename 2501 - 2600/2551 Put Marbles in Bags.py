7350337b00bd26d401c68a701bde0f33e7c2ae7c
Md Abedin <mdabedincs@gmail.com>
2025-03-30 23:41:42 -0400

    update 2551

--- 2501 - 2600/2551 Put Marbles in Bags.py
+++ 2501 - 2600/2551 Put Marbles in Bags.py
@@ -1,5 +1,4 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        if k == 1: return 0
-        pair_sums = sorted(list(map(sum, pairwise(weights))))
-        return sum(pair_sums[-(k-1):]) - sum(pair_sums[:k-1])
+        pair_sums = sorted(list(map(sum,pairwise(weights))))
+        return sum(pair_sums[len(pair_sums)-k+1:])-sum(pair_sums[:k-1])
