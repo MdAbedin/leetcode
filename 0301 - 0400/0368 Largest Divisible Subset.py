3b354001cd054152d483efee754988ae9f4fb70f
Md Abedin <mdabedincs@gmail.com>
2025-04-05 23:26:11 -0400

    update 0368

--- 0301 - 0400/0368 Largest Divisible Subset.py
+++ 0301 - 0400/0368 Largest Divisible Subset.py
@@ -1,24 +1,8 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
-        
         nums.sort()
-        dp = [[1,-1] for num in nums]
-        ans_len, ans_i = 1, 0
-        
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i]%nums[j] == 0:
-                    dp[i] = [max(dp[i][0], dp[j][0]+1), j if max(dp[i][0], dp[j][0]+1) > dp[i][0] else dp[i][1]]
-                    
-                    if dp[i][0] > ans_len:
-                        ans_len = dp[i][0]
-                        ans_i = i
-                        
-        ans = set()
-        while ans_i != -1:
-            ans.add(nums[ans_i])
-            ans_i = dp[ans_i][1]
-            
-        return list(ans)
+
+        @cache
+        def solve(i): return [nums[i]] + max((solve(i2) for i2 in range(i+1,len(nums)) if nums[i2]%nums[i] == 0),default=[],key=len)
+
+        return max(map(solve,range(len(nums))),key=len)
