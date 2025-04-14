ce928f3322d31bfe02fc383da0037d49f460d24d
Md Abedin <mdabedincs@gmail.com>
2025-04-14 13:22:51 -0400

    update 1534

--- 1501 - 1600/1534 Count Good Triplets.py
+++ 1501 - 1600/1534 Count Good Triplets.py
@@ -1,10 +1,3 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
-        ans = 0
-        for i in range(len(arr)):
-            for j in range(i+1, len(arr)):
-                for k in range(j+1, len(arr)):
-                    if abs(arr[j]-arr[i]) <= a and abs(arr[k]-arr[j])<=b and abs(arr[k]-arr[i]) <= c:
-                        ans += 1
-                        
-        return ans
+        return sum(abs(x1-x2) <= a and abs(x2-x3) <= b and abs(x1-x3) <= c for x1,x2,x3 in combinations(arr,3))
