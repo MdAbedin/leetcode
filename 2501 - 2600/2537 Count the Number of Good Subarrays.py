010f41a4cebc167c2d59017dfacb5e3196b9150e
Md Abedin <mdabedincs@gmail.com>
2025-04-15 20:10:18 -0400

    update 2537

--- 2501 - 2600/2537 Count the Number of Good Subarrays.py
+++ 2501 - 2600/2537 Count the Number of Good Subarrays.py
@@ -1,19 +1,18 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        good = 0
-        i = 0
-        ans = 0
         counts = Counter()
-        
-        for j in range(len(nums)):
-            good += counts[nums[j]]
-            counts[nums[j]] += 1
-            
-            while good - (counts[nums[i]]-1) >= k:
-                counts[nums[i]] -= 1
-                good -= counts[nums[i]]
-                i += 1
-            
-            if good >= k: ans += i+1
-        
+        good = 0
+        r = 0
+        ans = 0
+
+        for l in range(len(nums)):
+            while r < len(nums) and good < k:
+                good += counts[nums[r]]
+                counts[nums[r]] += 1
+                r += 1
+
+            if good >= k: ans += len(nums)-r+1
+            good -= counts[nums[l]]-1
+            counts[nums[l]] -= 1
+
         return ans
